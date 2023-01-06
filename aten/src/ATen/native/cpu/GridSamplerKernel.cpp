0b29a34877852a404f13b64a494da08ec6237d1c
Radek Bartoň <radek.barton@microsoft.com>
2023-01-06 17:14:47 +0100

    Fix code compatibility with MSVC

--- aten/src/ATen/native/cpu/GridSamplerKernel.cpp
+++ aten/src/ATen/native/cpu/GridSamplerKernel.cpp
@@ -528,10 +528,10 @@ struct ApplyGridSample<scalar_t, 2, GridSamplerInterpolation::Bilinear,
                                 : (i_x_e > iVec(-1)) & (i_x_e < iVec(inp_W));
     auto s_mask = must_in_bound ? (i_y_s < iVec(inp_H))
                                 : (i_y_s > iVec(-1)) & (i_y_s < iVec(inp_H));
-    auto nw_mask = cast<scalar_t>(must_in_bound ? iVec(-1) : (w_mask & n_mask));
-    auto ne_mask = cast<scalar_t>(e_mask & n_mask);
-    auto sw_mask = cast<scalar_t>(w_mask & s_mask);
-    auto se_mask = cast<scalar_t>(e_mask & s_mask);
+    auto nw_mask = at::vec::cast<scalar_t>(must_in_bound ? iVec(-1) : (w_mask & n_mask));
+    auto ne_mask = at::vec::cast<scalar_t>(e_mask & n_mask);
+    auto sw_mask = at::vec::cast<scalar_t>(w_mask & s_mask);
+    auto se_mask = at::vec::cast<scalar_t>(e_mask & s_mask);
 
     return std::make_tuple(
       n, s, w, e,
