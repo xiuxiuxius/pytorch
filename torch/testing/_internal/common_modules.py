8336b3b3e82680b58e80c517dd0aed6d0fe526a5
samdow <samdow@fb.com>
2023-01-09 10:53:41 -0500

    [decompositions] add RNN decomp and testing
    ghstack-source-id: a10fa435d8061ece8101420bc28489915485a33f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91123

--- torch/testing/_internal/common_modules.py
+++ torch/testing/_internal/common_modules.py
@@ -1163,6 +1163,9 @@ module_db: List[ModuleInfo] = [
                                 dtypes=(torch.chalf,), device_type='cuda'),
                    # Ref: https://github.com/pytorch/pytorch/issues/73502
                    DecorateInfo(unittest.expectedFailure, 'TestModule', 'test_pickle', dtypes=(torch.chalf,)),
+                   # "slow_conv_transpose1d_out_cpu" not implemented for 'Half'
+                   DecorateInfo(unittest.expectedFailure, 'TestDecomp', 'test_module',
+                                dtypes=(torch.complex32,), device_type='cpu'),
                ),
                decorators=(
                    DecorateInfo(precisionOverride({torch.float32: 1e-04}), 'TestModule', 'test_memory_format'),
@@ -1201,6 +1204,9 @@ module_db: List[ModuleInfo] = [
                                 dtypes=(torch.chalf,), device_type='cuda'),
                    # Ref: https://github.com/pytorch/pytorch/issues/73502
                    DecorateInfo(unittest.expectedFailure, 'TestModule', 'test_pickle', dtypes=(torch.chalf,)),
+                   # "slow_conv_transpose2d_out_cpu" not implemented for 'Half'
+                   DecorateInfo(unittest.expectedFailure, 'TestDecomp', 'test_module',
+                                dtypes=(torch.complex32,), device_type='cpu'),
                ),
                decorators=(
                    DecorateInfo(precisionOverride({torch.float32: 1e-04}), 'TestModule', 'test_memory_format'),
@@ -1237,6 +1243,9 @@ module_db: List[ModuleInfo] = [
                                 dtypes=(torch.chalf,), device_type='cuda'),
                    # Ref: https://github.com/pytorch/pytorch/issues/73502
                    DecorateInfo(unittest.expectedFailure, 'TestModule', 'test_pickle', dtypes=(torch.chalf,)),
+                   # "slow_conv_transpose3d_out_cpu" not implemented for 'Half'
+                   DecorateInfo(unittest.expectedFailure, 'TestDecomp', 'test_module',
+                                dtypes=(torch.complex32,), device_type='cpu'),
                ),
                decorators=(
                    DecorateInfo(precisionOverride({torch.float32: 1e-04}), 'TestModule', 'test_memory_format'),
